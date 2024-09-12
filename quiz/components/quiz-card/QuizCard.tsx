124b0a0e6cbc327d7a195deb8641d74349a5fe4f
Vitali Zhybuliou <zhybuliou.v.v@gmail.com>
2024-09-12 17:54:36 +0300

    feat: add style

--- quiz/components/quiz-card/QuizCard.tsx
+++ quiz/components/quiz-card/QuizCard.tsx
@@ -6,7 +6,7 @@ import QuestionBlock from './QuestionBlock'
 const QuizCard = () => {
 
   return (
-    <div className='max-w-[800px] min-h-[500px] mx-auto bg-secondary mt-8 rounded-lg shadow-lg mx-2'>
+    <div className='max-w-[800px] min-h-[500px] md:mx-auto bg-secondary mt-8 rounded-lg shadow-lg mx-2'>
       <QuestionBlock />
     </div>
   )
